2a81187cc00eeb994a468c784828a4479a316bc2
UnSensei <chakradhar.raju10@gmail.com>
2022-11-14 15:17:46 +0530

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Check this app live here => https://golden-mooncake-ee46ea.netlify.app/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/DragableGraph.js
+++ src/DragableGraph.js
@@ -1,21 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 import dragable from "chartjs-plugin-dragdata";
 import zoomer from "chartjs-plugin-zoom";
 Chart.register(...registerables, dragable, zoomer);
-
-// [{ x: 5, y: 50 }],
-//     [{ x: 7, y: -100 }],
-//     [
-//       { x: 1, y: 0 - 200 },
-//       { x: 5, y: 200 },
-//     ],
-//     [
-//       { x: 1, y: 500 },
-//       { x: 5, y: -500 },
-//       { x: 7, y: 500 },
-//     ],
 
 export const ChartGraph = ({ data, configOptions }) => {
   const [lineData, setLineData] = useState(data);
